96267303b7ea2dacc9bed0b466f494b698d595e5
Viral Patel <viral.patel@fourth.com>
2018-07-24 15:11:41 +0100

    add check file

--- unzip/checkfile.go
+++ unzip/checkfile.go
@@ -5,6 +5,16 @@ import (
 	"os"
 )
 
+func checkfile(src string) error {
+	if _, err := os.Stat(src); os.IsNotExist(err) {
+		fmt.Println("File Does Not Exist")
+	} else {
+		deletefile(src)
+	}
+
+	return nil
+}
+
 func deletefile(src string) error {
 	var err = os.Remove(src)
 
