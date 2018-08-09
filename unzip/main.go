a1f6e1fc502fcf1652359c94d03f557857c855cc
Viral Patel <viral.patel@fourth.com>
2018-08-09 16:46:49 +0100

    ammending the main.go

--- unzip/main.go
+++ unzip/main.go
@@ -8,10 +8,11 @@ func main() {
 
 	version := flag.String("version", "", "versionofterraform")
 	//zipfile := flag.String("zipfile", "", "zipfilepath")
-	//extract := flag.String("extract", "", "extractdestination")
+	extract := flag.String("extract", "", "extractdestination")
 	path := flag.String("path", "", "downloadpath")
 	flag.Parse()
 
 	downloadversion(*version, *path)
+	unzip(*path, *extract)
 
 }
